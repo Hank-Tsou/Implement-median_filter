8e6110fe7902f5fdbe34cb7edbdf4066abfceab2
Hank-Tsou <hank630280888@gmail.com>
2019-07-14 11:57:28 +0800

    median filter

--- median_filter.py
+++ median_filter.py
@@ -1,55 +1,68 @@
-import pylab as pl	#matplotlib's subpackage as pl use for graph
-import numpy as np	#use numpy library as np for array object
-import cv2			#opencv-python
 
-def filtering(padding_mage, size):	#median filtering function (padding image, size of filter)
-    
-    final = np.zeros((x, y), dtype='i')					# create a metrix the size same as grayimg image
-    m_filter = np.zeros((size*size), dtype='i')			# create the filter array
-     
-    for count_pixel_x in range(((size-1)/2), x+1):		#count grayimg pixel on padding image
-        for count_pixel_y in range(((size-1)/2), y+1):	#count grayimg pixel on padding image
+#------------------------------------#
+# Author: Yueh-Lin Tsou              #
+# Update: 7/10/2019                  #
+# E-mail: hank630280888@gmail.com    #
+#------------------------------------#
+
+"""---------------------------
+Implement Median Filtering
+---------------------------"""
+
+# Import OpenCV Library, numpy and command line interface
+import cv2
+import numpy as np
+import argparse
+from matplotlib import pyplot as plt
+
+# -------------------------- Median filter function -------------------------- #
+
+def filtering(grayimg, padding_mage, size):	#median filtering function (padding image, size of filter)
+    x, y = grayimg.shape
+    final = np.zeros((x, y), dtype='i') # create a metrix the size same as grayimg image
+    m_filter = np.zeros((size*size), dtype='i') # create the filter array
+
+    for count_pixel_x in range(((size-1)//2), x+1):		#count grayimg pixel on padding image
+        for count_pixel_y in range(((size-1)//2), y+1):	#count grayimg pixel on padding image
             count_filter = 0							# filter counter in order to fill the pixel in the filter to sort and find the median value
-            for count_compare_pixel_x in range(count_pixel_x-((size-1)/2), count_pixel_x+((size-1)/2)+1):		# count the filter position
-                for count_compare_pixel_y in range(count_pixel_y-((size-1)/2), count_pixel_y+((size-1)/2)+1):	# count the filter position
+            for count_compare_pixel_x in range(count_pixel_x-((size-1)//2), count_pixel_x+((size-1)//2)+1):		# count the filter position
+                for count_compare_pixel_y in range(count_pixel_y-((size-1)//2), count_pixel_y+((size-1)//2)+1):	# count the filter position
                         m_filter[count_filter] = padding_mage[count_compare_pixel_x, count_compare_pixel_y]		#fill the image value in the filter
                         count_filter = count_filter+1	# move to next filter value
-            
+
             m_filter.sort()	# sort the value in the filter
-            final[count_pixel_x-((size-1)/2), count_pixel_y-((size-1)/2)]=m_filter[((size*size)-1)/2]			# add the median value in the new image
+            final[count_pixel_x-((size-1)//2), count_pixel_y-((size-1)//2)]=m_filter[((size*size)-1)//2]			# add the median value in the new image
 
-    return final	#return new image
+    return final #return new image
 
-### main
-image_name = input ('image name(with .png .jpg): ')		# input image name
+# -------------------------- main -------------------------- #
+if __name__ == '__main__':
+    # read one input from terminal
+    # (1) command line >> python re.py -i input_image.jpg -s 3
 
-filter_size = input ('filter size (odd number): ')		# input filter size
-while filter_size % 2 == 0:								# while input filter size is an even number re-input
-    filter_size = input ('filter size (odd number): ')	# input filter size
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path to the input image")
+    ap.add_argument("-s", "--fsize", required=True, help="input filter size (odd)")
+    args = vars(ap.parse_args())
 
-img = cv2.imread(image_name)							# read image
-grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)			# convert image to grayscal
+    # Read image
+    image = cv2.imread(args["image"])
+    fsize = int(args["fsize"])
 
-### padding image with zero: set all pixels outside the grayimg image to 0
-x, y = grayimg.shape																# get image size x*y, for a image with x rows and y columns
-padding_mage = np.zeros((x+(filter_size-1), y+(filter_size-1)), dtype = 'i')		#create a zero metrix the size of zero depend on the size of image and filter											
+    boarder = (fsize-1)//2
 
-for i in range((filter_size-1)/2, x+((filter_size-1)/2)):							# count the x-position where grayimg pixel fill in 
-    for j in range((filter_size-1)/2, y+((filter_size-1)/2)):						# count the y-position where grayimg pixel fill in 
-        padding_mage[i, j] = grayimg[i-((filter_size-1)/2), j-((filter_size-1)/2)]	# add the image in the center of the zero metrix
-																					# so now we create a padding image with zero
+    grayimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    padding_mage= cv2.copyMakeBorder(grayimg,boarder,boarder,boarder,boarder,cv2.BORDER_CONSTANT,value=0)
 
-final = filtering(padding_mage, filter_size)	#call function to do median filtering
+    final = filtering(grayimg, padding_mage, fsize)	#call function to do median filtering
 
-### show original and blurring image
-pl.subplot(121)				# image position 1 row, 2 column, first position
-pl.imshow(grayimg)			# show image "grayimg"
-pl.title('original image')	# graph title "original image"
-pl.set_cmap('gray')			# show in gray scale
-
-pl.subplot(122)				# image position 1 row, 2 column, second position
-pl.imshow(final)			# show image "final"
-pl.title('result image')	# graph title "result image"
-pl.set_cmap('gray')			# show in gray scale
-
-pl.show()	                # output image
+    ### show original and result image
+    plt.subplot(121)
+    plt.imshow(grayimg)
+    plt.title('original image')
+    plt.set_cmap('gray')
+    plt.subplot(122)
+    plt.imshow(final)
+    plt.title('result image')
+    plt.set_cmap('gray')
+    plt.show()
